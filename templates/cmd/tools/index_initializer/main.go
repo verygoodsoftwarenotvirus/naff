5796bcbc1932f7f87e3fd50a73e5f03d1935892f
Jeffrey D <verygoodsoftwarenotvirus@protonmail.com>
2021-05-31 11:00:12 -0500

    cmd folder done

--- templates/cmd/tools/index_initializer/main.go
+++ templates/cmd/tools/index_initializer/main.go
@@ -3,34 +3,11 @@ package indexinitializer
 import (
 	"bytes"
 	_ "embed"
-	"path/filepath"
 
 	"gitlab.com/verygoodsoftwarenotvirus/naff/forks/jennifer/jen"
 	"gitlab.com/verygoodsoftwarenotvirus/naff/lib/constants"
-	"gitlab.com/verygoodsoftwarenotvirus/naff/lib/utils"
 	"gitlab.com/verygoodsoftwarenotvirus/naff/models"
 )
-
-const (
-	packageName = "main"
-
-	basePackagePath = "cmd/tools/index_initializer"
-)
-
-// RenderPackage renders the package
-func RenderPackage(proj *models.Project) error {
-	stringFiles := map[string]string{
-		"main.go": mainDotGoString(proj),
-	}
-
-	for path, file := range stringFiles {
-		if err := utils.RenderStringFile(proj, filepath.Join(basePackagePath, path), file); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
 
 //go:embed main.gotpl
 var mainTemplate string
