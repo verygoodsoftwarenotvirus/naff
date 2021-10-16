5425b3e7e4bf46a9a6beb290b31c9dab882d9e7e
Jeffrey <git@verygoodsoftwarenotvirus.ru>
2021-10-16 13:33:34 -0500

    pre-updates worker tests have no trace of items

--- templates/_internal_/workers/pre_updates_worker_test_.go
+++ templates/_internal_/workers/pre_updates_worker_test_.go
@@ -165,11 +165,12 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 	}
 
 	for _, typ := range proj.DataTypes {
-		_ = typ
+		sn := typ.Name.Singular()
+		scn := typ.Name.SingularCommonName()
 
 		testCases = append(testCases,
 			jen.ID("T").Dot("Run").Call(
-				jen.Lit("with ItemDataType"),
+				jen.Litf("with %sDataType", sn),
 				jen.Func().Params(jen.ID("t").Op("*").Qual("testing", "T")).Body(
 					jen.ID("t").Dot("Parallel").Call(),
 					jen.Newline(),
@@ -178,7 +179,7 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 					jen.ID("client").Op(":=").Op("&").Qual("net/http", "Client").Values(),
 					jen.Newline(),
 					jen.ID("body").Op(":=").Op("&").Qual(proj.TypesPackage(), "PreUpdateMessage").Valuesln(
-						jen.ID("DataType").MapAssign().Qual(proj.TypesPackage(), "ItemDataType"), jen.ID("Item").MapAssign().Qual(proj.FakeTypesPackage(), "BuildFakeItem").Call()),
+						jen.ID("DataType").MapAssign().Qualf(proj.TypesPackage(), "%sDataType", sn), jen.ID(sn).MapAssign().Qualf(proj.FakeTypesPackage(), "BuildFake%s", sn).Call()),
 					jen.List(jen.ID("examplePayload"), jen.ID("err")).Op(":=").Qual("encoding/json", "Marshal").Call(jen.ID("body")),
 					jen.ID("require").Dot("NoError").Call(
 						jen.ID("t"),
@@ -186,18 +187,18 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 					),
 					jen.Newline(),
 					jen.ID("dbManager").Op(":=").Qual(proj.DatabasePackage(), "BuildMockDatabase").Call(),
-					jen.ID("dbManager").Dot("ItemDataManager").Dot("On").Callln(
-						jen.Lit("UpdateItem"),
+					jen.ID("dbManager").Dotf("%sDataManager", sn).Dot("On").Callln(
+						jen.Litf("Update%s", sn),
 						jen.Qual(proj.TestUtilsPackage(), "ContextMatcher"),
-						jen.ID("body").Dot("Item"),
+						jen.ID("body").Dot(sn),
 					).Dot("Return").Call(jen.Nil()),
 					jen.Newline(),
 					jen.ID("searchIndexManager").Op(":=").Op("&").Qual(proj.InternalSearchPackage("mock"), "IndexManager").Values(),
 					jen.ID("searchIndexManager").Dot("On").Callln(
 						jen.Lit("Index"),
 						jen.Qual(proj.TestUtilsPackage(), "ContextMatcher"),
-						jen.ID("body").Dot("Item").Dot("ID"),
-						jen.ID("body").Dot("Item"),
+						jen.ID("body").Dot(sn).Dot("ID"),
+						jen.ID("body").Dot(sn),
 					).Dot("Return").Call(jen.Nil()),
 					jen.Newline(),
 					jen.ID("searchIndexLocation").Op(":=").Qual(proj.InternalSearchPackage(), "IndexPath").Call(jen.ID("t").Dot("Name").Call()),
@@ -247,7 +248,7 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 			),
 			jen.Newline(),
 			jen.ID("T").Dot("Run").Call(
-				jen.Lit("with ItemDataType with error updating item"),
+				jen.Litf("with %sDataType with error updating %s", sn, scn),
 				jen.Func().Params(jen.ID("t").Op("*").Qual("testing", "T")).Body(
 					jen.ID("t").Dot("Parallel").Call(),
 					jen.Newline(),
@@ -256,7 +257,7 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 					jen.ID("client").Op(":=").Op("&").Qual("net/http", "Client").Values(),
 					jen.Newline(),
 					jen.ID("body").Op(":=").Op("&").Qual(proj.TypesPackage(), "PreUpdateMessage").Valuesln(
-						jen.ID("DataType").MapAssign().Qual(proj.TypesPackage(), "ItemDataType"), jen.ID("Item").MapAssign().Qual(proj.FakeTypesPackage(), "BuildFakeItem").Call(),
+						jen.ID("DataType").MapAssign().Qualf(proj.TypesPackage(), "%sDataType", sn), jen.ID(sn).MapAssign().Qualf(proj.FakeTypesPackage(), "BuildFake%s", sn).Call(),
 					),
 					jen.List(jen.ID("examplePayload"), jen.ID("err")).Op(":=").Qual("encoding/json", "Marshal").Call(jen.ID("body")),
 					jen.ID("require").Dot("NoError").Call(
@@ -265,10 +266,10 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 					),
 					jen.Newline(),
 					jen.ID("dbManager").Op(":=").Qual(proj.DatabasePackage(), "BuildMockDatabase").Call(),
-					jen.ID("dbManager").Dot("ItemDataManager").Dot("On").Callln(
-						jen.Lit("UpdateItem"),
+					jen.ID("dbManager").Dotf("%sDataManager", sn).Dot("On").Callln(
+						jen.Litf("Update%s", sn),
 						jen.Qual(proj.TestUtilsPackage(), "ContextMatcher"),
-						jen.ID("body").Dot("Item"),
+						jen.ID("body").Dot(sn),
 					).Dot("Return").Call(jen.Qual("errors", "New").Call(jen.Lit("blah"))),
 					jen.Newline(),
 					jen.ID("searchIndexLocation").Op(":=").Qual(proj.InternalSearchPackage(), "IndexPath").Call(jen.ID("t").Dot("Name").Call()),
@@ -313,7 +314,7 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 			),
 			jen.Newline(),
 			jen.ID("T").Dot("Run").Call(
-				jen.Lit("with ItemDataType and error updating search index"),
+				jen.Litf("with %sDataType and error updating search index", sn),
 				jen.Func().Params(jen.ID("t").Op("*").Qual("testing", "T")).Body(
 					jen.ID("t").Dot("Parallel").Call(),
 					jen.Newline(),
@@ -322,7 +323,7 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 					jen.ID("client").Op(":=").Op("&").Qual("net/http", "Client").Values(),
 					jen.Newline(),
 					jen.ID("body").Op(":=").Op("&").Qual(proj.TypesPackage(), "PreUpdateMessage").Valuesln(
-						jen.ID("DataType").MapAssign().Qual(proj.TypesPackage(), "ItemDataType"), jen.ID("Item").MapAssign().Qual(proj.FakeTypesPackage(), "BuildFakeItem").Call()),
+						jen.ID("DataType").MapAssign().Qualf(proj.TypesPackage(), "%sDataType", sn), jen.ID(sn).MapAssign().Qualf(proj.FakeTypesPackage(), "BuildFake%s", sn).Call()),
 					jen.List(jen.ID("examplePayload"), jen.ID("err")).Op(":=").Qual("encoding/json", "Marshal").Call(jen.ID("body")),
 					jen.ID("require").Dot("NoError").Call(
 						jen.ID("t"),
@@ -330,18 +331,18 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 					),
 					jen.Newline(),
 					jen.ID("dbManager").Op(":=").Qual(proj.DatabasePackage(), "BuildMockDatabase").Call(),
-					jen.ID("dbManager").Dot("ItemDataManager").Dot("On").Callln(
-						jen.Lit("UpdateItem"),
+					jen.ID("dbManager").Dotf("%sDataManager", sn).Dot("On").Callln(
+						jen.Litf("Update%s", sn),
 						jen.Qual(proj.TestUtilsPackage(), "ContextMatcher"),
-						jen.ID("body").Dot("Item"),
+						jen.ID("body").Dot(sn),
 					).Dot("Return").Call(jen.Nil()),
 					jen.Newline(),
 					jen.ID("searchIndexManager").Op(":=").Op("&").Qual(proj.InternalSearchPackage("mock"), "IndexManager").Values(),
 					jen.ID("searchIndexManager").Dot("On").Callln(
 						jen.Lit("Index"),
 						jen.Qual(proj.TestUtilsPackage(), "ContextMatcher"),
-						jen.ID("body").Dot("Item").Dot("ID"),
-						jen.ID("body").Dot("Item"),
+						jen.ID("body").Dot(sn).Dot("ID"),
+						jen.ID("body").Dot(sn),
 					).Dot("Return").Call(jen.Qual("errors", "New").Call(jen.Lit("blah"))),
 					jen.Newline(),
 					jen.ID("postArchivesPublisher").Op(":=").Op("&").Qual(proj.InternalMessageQueuePublishersPackage("mock"), "Publisher").Values(),
@@ -387,7 +388,7 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 			),
 			jen.Newline(),
 			jen.ID("T").Dot("Run").Call(
-				jen.Lit("with ItemDataType and error publishing data change event"),
+				jen.Litf("with %sDataType and error publishing data change event", sn),
 				jen.Func().Params(jen.ID("t").Op("*").Qual("testing", "T")).Body(
 					jen.ID("t").Dot("Parallel").Call(),
 					jen.Newline(),
@@ -396,7 +397,7 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 					jen.ID("client").Op(":=").Op("&").Qual("net/http", "Client").Values(),
 					jen.Newline(),
 					jen.ID("body").Op(":=").Op("&").Qual(proj.TypesPackage(), "PreUpdateMessage").Valuesln(
-						jen.ID("DataType").MapAssign().Qual(proj.TypesPackage(), "ItemDataType"), jen.ID("Item").MapAssign().Qual(proj.FakeTypesPackage(), "BuildFakeItem").Call()),
+						jen.ID("DataType").MapAssign().Qualf(proj.TypesPackage(), "%sDataType", sn), jen.ID(sn).MapAssign().Qualf(proj.FakeTypesPackage(), "BuildFake%s", sn).Call()),
 					jen.List(jen.ID("examplePayload"), jen.ID("err")).Op(":=").Qual("encoding/json", "Marshal").Call(jen.ID("body")),
 					jen.ID("require").Dot("NoError").Call(
 						jen.ID("t"),
@@ -404,18 +405,18 @@ func buildTestPreUpdatesWorker_HandleMessage(proj *models.Project) []jen.Code {
 					),
 					jen.Newline(),
 					jen.ID("dbManager").Op(":=").Qual(proj.DatabasePackage(), "BuildMockDatabase").Call(),
-					jen.ID("dbManager").Dot("ItemDataManager").Dot("On").Callln(
-						jen.Lit("UpdateItem"),
+					jen.ID("dbManager").Dotf("%sDataManager", sn).Dot("On").Callln(
+						jen.Litf("Update%s", sn),
 						jen.Qual(proj.TestUtilsPackage(), "ContextMatcher"),
-						jen.ID("body").Dot("Item"),
+						jen.ID("body").Dot(sn),
 					).Dot("Return").Call(jen.Nil()),
 					jen.Newline(),
 					jen.ID("searchIndexManager").Op(":=").Op("&").Qual(proj.InternalSearchPackage("mock"), "IndexManager").Values(),
 					jen.ID("searchIndexManager").Dot("On").Callln(
 						jen.Lit("Index"),
 						jen.Qual(proj.TestUtilsPackage(), "ContextMatcher"),
-						jen.ID("body").Dot("Item").Dot("ID"),
-						jen.ID("body").Dot("Item"),
+						jen.ID("body").Dot(sn).Dot("ID"),
+						jen.ID("body").Dot(sn),
 					).Dot("Return").Call(jen.Nil()),
 					jen.Newline(),
 					jen.ID("searchIndexLocation").Op(":=").Qual(proj.InternalSearchPackage(), "IndexPath").Call(jen.ID("t").Dot("Name").Call()),
